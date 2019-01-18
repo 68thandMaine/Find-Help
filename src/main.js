d18a3d5824c05daf88cfc350820933d9d1387a3d
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-18 09:08:41 -0800

    Add import statements connecting lages

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { Doctors } from './doctors.js';
 import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
---
+++ src/ui-logic.js
@@ -0,0 +1,2 @@
+import $ from 'jquery';
+import { Doctors } from './doctors.js';
