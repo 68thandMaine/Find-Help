01cf0e21873135a5faf34f7084dbbd743f26556b
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-18 09:22:18 -0800

    Set up API call for getting a list of doctors

--- src/doctors.js
+++ src/doctors.js
@@ -1,6 +1,26 @@
 class Doctors {
+
+  // ----- API call to retrieve a list of 10 doctors based off user parameters------------------------------//
   getDoctors(issue, location, gender) {
     const Promse = require('es6-promise').Promise;
+    return new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequst();
+      const url = `
+      https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=${location}&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      reques.send();
+    });
   }
+//--------------------------------//
+
+
+
 }
 exports { Doctors }
