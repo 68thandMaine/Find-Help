9744ba6260c66f094f8745655dd522f24720f627
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-19 16:29:00 -0800

    Add a function that takes an array and creates option tags for the select input form with each option holding the name of a illness/condition

--- src/main.js
+++ src/main.js
@@ -4,7 +4,24 @@ import $ from 'jquery';
 import './styles.css';
 import 'bootstrap';
 
+function getConditions() {
+  const knownConditions = new KnownConditions();
+  let conditionsPromise = knownConditions.condtions();
+
+  conditionsPromise.then((response) => {
+    knownCondtions.knownConditionsList(response);
+    buildConditionOptions(knownCondtions.listOfConditions);
+  }),
+}
+
+function buildConditionOptions(illnesses) {
+  illnesses.forEach((illness) => {
+    $('#conditionSelection').append(`<option value="${illness}">${illness}</option>`);
+  });
+}
+
 $(document).ready(function() {
+  getConditions();
   const doctors = new Doctors();
 
 
@@ -12,11 +29,13 @@ $(document).ready(function() {
 
 $('.searchForm').submit(function(event) {
   event.preventDefault();
-  let medicalIssue = $('#concern').val();
-  $('#concern').val('');
-  let gender = $('#gender').val();
+  let medicalIssue = $('#conditionSelection').val();
+  $('#conditionSelection').val('');
+  let gender = $('#genderSelection').val();
   $('#gender').val('');
-
+if($("#conditionSelection").val() || $("#genderSelection").val() === ' ') {
+  return displayError
+}
   let getListOfDoctorsPromise = doctors.getDoctors(medicalIssue, gender);
   getListOfDoctorsPromise.then(function(response) {
     let body = JSON.parse(response);
