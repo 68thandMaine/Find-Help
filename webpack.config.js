127927b54b9d752bfeb3c81bda2c1c35be7fb4a2
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-20 21:23:31 -0800

    Add configuration for file loader'

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,23 @@ module.exports = {
         options: {
           presets: ['es2015']
         }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ],
       }
     ]
-  },
-};
+    },
+  };
