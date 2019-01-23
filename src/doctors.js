3728bafe9b8c686fdc5867d38f12bcf1b4cd7fda
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-22 20:52:18 -0800

    Remove search by gender and replace it with a name search

--- src/doctors.js
+++ src/doctors.js
@@ -6,12 +6,12 @@ constructor(firstName, lastName, id) {
   this.id = id;
 }
 
-  getDoctors(issue, gender) {
+  getDoctors(issue, name) {
     const Promse = require('es6-promise').Promise;
     return new Promise(function(resolve, reject) {
       const request = new XMLHttpRequest();
       const url =
-      `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&gender=${gender}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
